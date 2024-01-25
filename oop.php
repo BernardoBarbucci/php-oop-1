3684fac3b3ba9f728134117142f32aba519500a6
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-25 22:30:24 +0100

    aggiunit altri film

--- oop.php
+++ oop.php
@@ -37,6 +37,11 @@ $nolanMovies = [
     new Movie("The Dark Knight", "Christopher Nolan", 2008, "Action", 152, 9.0, ["Christian Bale", "Heath Ledger"], "Batman faces the Joker's chaos", "English", "$185 million"),
     new Movie("Interstellar", "Christopher Nolan", 2014, "Sci-Fi", 169, 8.6, ["Matthew McConaughey", "Anne Hathaway"], "Space voyage to save humanity", "English", "$165 million"),
     new Movie("Dunkirk", "Christopher Nolan", 2017, "War", 106, 7.8, ["Fionn Whitehead", "Tom Hardy"], "Evacuation of Allied soldiers during World War II", "English", "$100 million"),
+    new Movie("Memento", "Christopher Nolan", 2000, "Mystery", 113, 8.4, ["Guy Pearce", "Carrie-Anne Moss"], "A man with short-term memory loss trying to solve a mystery", "English", "$9 million"),
+    new Movie("The Prestige", "Christopher Nolan", 2006, "Drama", 130, 8.5, ["Christian Bale", "Hugh Jackman"], "Rival magicians engage in a competitive rivalry", "English", "$40 million"),
+    new Movie("Batman Begins", "Christopher Nolan", 2005, "Action", 140, 8.2, ["Christian Bale", "Liam Neeson"], "Bruce Wayne becomes Batman", "English", "$150 million"),
+    new Movie("The Dark Knight Rises", "Christopher Nolan", 2012, "Action", 164, 8.4, ["Christian Bale", "Tom Hardy"], "Batman faces a new enemy, Bane", "English", "$230 million"),
+    new Movie("Tenet", "Christopher Nolan", 2020, "Action", 150, 7.4, ["John David Washington", "Robert Pattinson"], "Time manipulation and inversion", "English", "$200 million"),
 ];
 
 $tarantinoMovies = [
@@ -44,6 +49,11 @@ $tarantinoMovies = [
     new Movie("Kill Bill: Vol. 1", "Quentin Tarantino", 2003, "Action", 111, 8.1, ["Uma Thurman", "Lucy Liu"], "The Bride seeks revenge against her former allies", "English", "$30 million"),
     new Movie("Django Unchained", "Quentin Tarantino", 2012, "Drama", 165, 8.4, ["Jamie Foxx", "Christoph Waltz"], "A freed slave sets out to rescue his wife", "English", "$100 million"),
     new Movie("Once Upon a Time in Hollywood", "Quentin Tarantino", 2019, "Comedy", 161, 7.6, ["Leonardo DiCaprio", "Brad Pitt"], "A fading actor and his stunt double navigate the changing industry", "English", "$90 million"),
+    new Movie("Reservoir Dogs", "Quentin Tarantino", 1992, "Crime", 99, 8.3, ["Harvey Keitel", "Tim Roth"], "A botched jewelry heist and its aftermath", "English", "$1.2 million"),
+    new Movie("Jackie Brown", "Quentin Tarantino", 1997, "Crime", 154, 7.5, ["Pam Grier", "Samuel L. Jackson"], "Flight attendant gets involved in smuggling money", "English", "$12 million"),
+    new Movie("The Hateful Eight", "Quentin Tarantino", 2015, "Mystery", 168, 7.8, ["Samuel L. Jackson", "Kurt Russell"], "Bounty hunters seek shelter during a blizzard", "English", "$44 million"),
+    new Movie("Kill Bill: Vol. 2", "Quentin Tarantino", 2004, "Action", 137, 8.0, ["Uma Thurman", "David Carradine"], "The Bride continues her quest for vengeance", "English", "$30 million"),
+    new Movie("Inglourious Basterds", "Quentin Tarantino", 2009, "Drama", 153, 8.3, ["Brad Pitt", "Christoph Waltz"], "A group of Jewish U.S. soldiers plot to kill Hitler", "English", "$70 million"),
 ];
 
 
@@ -88,9 +98,9 @@ function displayMovieInfo($movie)
 
 <body>
 
-    <main>
+    <main class="container fluid d-flex flex-wrap">
         <?php foreach ($nolanMovies as $movie) : ?>
-            <div class="card" style="width: 18rem;">
+            <div class="card m-2" style="width: 18rem;">
                 <!-- <img src="..." class="card-img-top" alt="movie img"> -->
                 <div class="card-body">
                     <h5 class="card-title text-center mb-4"><?php echo $movie->title; ?></h5>
@@ -119,7 +129,7 @@ function displayMovieInfo($movie)
         <?php endforeach; ?>
 
         <?php foreach ($tarantinoMovies as $movie) : ?>
-            <div class="card" style="width: 18rem;">
+            <div class="card  m-2" style="width: 18rem;">
                 <!-- <img src="..." class="card-img-top" alt="movie img"> -->
                 <div class="card-body">
                     <h5 class="card-title text-center mb-4"><?php echo $movie->title; ?></h5>
