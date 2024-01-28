f6526b03e0f1a22bb0bba9abdb7c1d9b7b2ae296
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-28 18:38:13 +0100

    aggiornata la funzione che prendeva solo il max, adesso ho applicato il parsing dei valori delle values per ottenere il rating min e max ->
    esercizio completato

--- oop.php
+++ oop.php
@@ -1,6 +1,4 @@
 <?php
-// esecizio ricominciato da capo 
-
 class MOVIE
 {
     // variabili d'istanza
@@ -78,14 +76,12 @@ function filterRating($movies, $ratingRange)
 {
     $filteredMovies = [];
 
+    list($min, $max) = explode('-', $ratingRange);
+
     foreach ($movies as $movie) {
         $rating = $movie->rating;
 
-        // max value e min value
-        list($min, $max) = explode('-', $ratingRange);
-        $averageRating = ($min + $max) / 2;
-
-        if ($rating >= $averageRating) {
+        if ($rating >= $min && $rating <= $max) {
             $filteredMovies[] = $movie;
         }
     }
@@ -112,7 +108,8 @@ if (isset($_GET['rating'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nolan and Tarantino's films</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body class="bg-dark">
@@ -122,10 +119,12 @@ if (isset($_GET['rating'])) {
                 Movies from Nolan and Tarantino
             </h1>
         </div>
-        <select class="form-select ms-2 mb-4" aria-label="Default select example" style="width: 15rem" onchange="filterMovies()">
+        <select class="form-select ms-2 mb-4" aria-label="Default select example" style="width: 15rem"
+            onchange="filterMovies()">
             <option selected>Filter by rating</option>
-            <option value="5-6">5-6</option>
+            <option value="6-7">6-7</option>
             <option value="7-8">7-8</option>
+            <option value="8-9">8-9</option>
             <option value="9-10">9-10</option>
         </select>
     </header>
@@ -135,72 +134,72 @@ if (isset($_GET['rating'])) {
         $filteredMovies = isset($_GET['rating']) ? filterRating([...$nolanMovies, ...$tarantinoMovies], $_GET['rating']) : [...$nolanMovies, ...$tarantinoMovies];
 
         foreach ($filteredMovies as $movie) : ?>
-            <div class="card m-2 bg-primary-subtle" style="width: 18rem;">
-                <!-- <img src="..." class="card-img-top" alt="movie img"> -->
-                <div class="card-body">
-                    <h5 class="card-title text-center mb-4"><?php echo $movie->title; ?></h5>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <p>
-                            Director: <?php echo $movie->director; ?>
-                        </p>
-                    </li>
-                    <li class="list-group-item">
-                        <p>
-                            Release date: <?php echo $movie->releaseYear ?>
-                        </p>
-                    </li>
-                    <li class="list-group-item">
-                        <p>
-                            Genre: <?php echo $movie->genre ?>
-                        </p>
-                    </li>
-                    <li class="list-group-item">
-                        <p>
-                            Rating: <?php echo $movie->rating ?>
-                        </p>
-                    </li>
-                    <li class="list-group-item">
-                        <p>
-                            Actors:
-                            <?php echo implode(", ", $movie->actors); ?>
-                        </p>
-                    </li>
-                    <li class="list-group-item">
-                        <p>
-                            Short trama: <?php echo $movie->plot ?>
-                        </p>
-                    </li>
-                    <li class="list-group-item">
-                        <p>
-                            Language: <?php echo $movie->language ?>
-                        </p>
-                    </li>
-                    <li class="list-group-item">
-                        <p>
-                            Budget to shoot the film: <?php echo $movie->budget ?>
-                        </p>
-                    </li>
-                </ul>
+        <div class="card m-2 bg-primary-subtle" style="width: 18rem;">
+            <!-- <img src="..." class="card-img-top" alt="movie img"> -->
+            <div class="card-body">
+                <h5 class="card-title text-center mb-4"><?php echo $movie->title; ?></h5>
             </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                    <p>
+                        Director: <?php echo $movie->director; ?>
+                    </p>
+                </li>
+                <li class="list-group-item">
+                    <p>
+                        Release date: <?php echo $movie->releaseYear ?>
+                    </p>
+                </li>
+                <li class="list-group-item">
+                    <p>
+                        Genre: <?php echo $movie->genre ?>
+                    </p>
+                </li>
+                <li class="list-group-item">
+                    <p>
+                        Rating: <?php echo $movie->rating ?>
+                    </p>
+                </li>
+                <li class="list-group-item">
+                    <p>
+                        Actors:
+                        <?php echo implode(", ", $movie->actors); ?>
+                    </p>
+                </li>
+                <li class="list-group-item">
+                    <p>
+                        Short trama: <?php echo $movie->plot ?>
+                    </p>
+                </li>
+                <li class="list-group-item">
+                    <p>
+                        Language: <?php echo $movie->language ?>
+                    </p>
+                </li>
+                <li class="list-group-item">
+                    <p>
+                        Budget to shoot the film: <?php echo $movie->budget ?>
+                    </p>
+                </li>
+            </ul>
+        </div>
         <?php endforeach; ?>
     </main>
 
     <!-- script per cambiare onclick i film e filtrarli quando viene selezionata una value nella select -->
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var select = document.querySelector('select');
+    document.addEventListener('DOMContentLoaded', function() {
+        var select = document.querySelector('select');
 
-            select.addEventListener('change', function() {
-                var selectedRating = select.value;
-                window.location.href = "?rating=" + selectedRating;
-            });
-
-            // Impostare il valore corrente della select
-            var currentRating = "<?php echo isset($_GET['rating']) ? $_GET['rating'] : '' ?>";
-            select.value = currentRating;
+        select.addEventListener('change', function() {
+            var selectedRating = select.value;
+            window.location.href = "?rating=" + selectedRating;
         });
+
+        // Impostare il valore corrente della select
+        var currentRating = "<?php echo isset($_GET['rating']) ? $_GET['rating'] : '' ?>";
+        select.value = currentRating;
+    });
     </script>
 </body>
 
